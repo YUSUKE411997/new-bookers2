7da218fa2fb9bb8bc5c22d3a46ec79507d287327
YUSUKE411997 <hy.skyblue.41@icloud.com>
2021-04-12 13:53:06 +0000

    [update]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,23 +1,42 @@
 class BooksController < ApplicationController
   
   def index
+    @book = Book.new
+    @books = Book.all
   end 
   
   def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to books_path
   end 
   
   def show
+    @book = Book.find(params[:id])
   end 
   
   def edit
+    @book = Book.find(params[:id])
   end 
   
   def update
+    book = Book.find(params[:id])
+    book.user_id = current_user.id
+    book.update(book_params)
+    redirect_to book_path
   end 
   
   def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
   end 
   
+  private
   
+  def book_params
+    params.require(:book).permit(:book_title, :book_body)
+  end 
   
 end
