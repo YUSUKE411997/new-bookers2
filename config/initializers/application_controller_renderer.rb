93d1bdb4aa4e800d2dafab1afecaa48d51d540bb
YUSUKE411997 <hy.skyblue.41@icloud.com>
2021-04-14 09:11:28 +0000

    [profile]show

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,7 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '7ebaa507912662a81b984483b0b19964727deac42bae2acb338f840bc1c322e6cd5acb2a97307d76e2cc22bca45d99f038037a6a89f5132815bb3fec4b5ffeed'
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   
   
+  
   devise_for :users
   root to: 'homes#top'
+  resources :users, only:[:index, :show, :edit, :update] 
   resources :books, only:[:index,:create,:show,:edit,:update,:destroy]
+
   
 end
+
