eb86c3a656a6d9997d26a6d2f0de679e12120ccf
YUSUKE411997 <hy.skyblue.41@icloud.com>
2021-04-15 06:43:57 +0000

    [start]lay

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,16 @@
 class UsersController < ApplicationController
   def index
     @users = User.all
+    @book = Book.new
   end
   
 
   def show
     @user = User.find(params[:id])
-    @books = @user.books.where(params[:id])
+    @book = Book.new
   end
   
+
 
   def edit
     @user = User.find(params[:id])
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   
   devise_for :users
   root to: 'homes#top'
+  get 'homes/about' => 'homes#about'
   resources :users, only:[:index, :show, :edit, :update] 
   resources :books, only:[:index,:create,:show,:edit,:update,:destroy]
 
