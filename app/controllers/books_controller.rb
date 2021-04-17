08616e2bc766500f836f629b17cfcb38bd26e5d8
YUSUKE411997 <hy.skyblue.41@icloud.com>
2021-04-17 11:14:11 +0000

    [finish]bookers2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,7 @@
 class BooksController < ApplicationController
   
+  before_action :ensure_correct_user, only: [:edit]
+  
   def index
     @book = Book.new
     @books = Book.all
@@ -55,5 +57,16 @@ class BooksController < ApplicationController
     params.require(:book).permit(:title, :body)
   end 
   
+  def ensure_correct_user
+    @book = Book.find(params[:id])
+    if @book.user != current_user
+      redirect_to books_path
+    end 
+  end
 end
+
+
+
+
+
 
